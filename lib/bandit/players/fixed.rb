765f8993395f2ed752b0173c0cd156e3bdd13880
Blake Carlson <blc@punchbowlsoftware.com>
2012-10-08 16:08:27 -0400

    Removed the storage tweaks for fixed player.

--- lib/bandit/players/fixed.rb
+++ lib/bandit/players/fixed.rb
@@ -4,11 +4,6 @@ module Bandit
     def choose_alternative(experiment)
       experiment.alternatives.first
     end
-
-    # store state variable by name
-    def set(name, value)
-      true
-    end
     
   end
 end
